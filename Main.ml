d791f37911a89eaec6513bb072ba7746e475ed63
Hellpi Mappa <baudetad@gmail.com>
2016-08-16 17:08:03 +0200

    Mise à jour

--- Main.ml
+++ Main.ml
@@ -22,9 +22,20 @@ type grid =
 	|Line of grid array
 	|Dot of bloc
 	
-	
+let texutre_uniform id = 
+	{front = id ;
+	back = id ;
+	left = id ;
+	right = id ;
+	up = id ;
+	down = id}
+
 let make_grid times length item =
 	let rec aux = function
 		|i when i = times -> Dot item
 		|i -> Line (Array.make length (aux (i + 1)))
 	in aux 0
+	
+let tex  = texutre_uniform 1
+let blo = {id = 1; texture = tex; shape = Bloc}
+let test = make_grid 2 2 blo
